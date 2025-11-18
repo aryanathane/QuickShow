e12469067ec02fcf553c2245f65a33cf46c8f9d1
aryanathane <143585933+aryanathane@users.noreply.github.com>
2025-11-18 22:33:40 +0530

    Day4 commit 2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,24 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { ClerkProvider } from "@clerk/clerk-react";
-// Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { AppProvider } from './context/AppContext.jsx'
+
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Add your Clerk Publishable Key to the .env file");
+  throw new Error('Missing Publishaable Key')
 }
 
-createRoot(document.getElementById("root")).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey='pk_test_Y2xpbWJpbmctb3Jpb2xlLTU2LmNsZXJrLmFjY291bnRzLmRldiQ'>
     <BrowserRouter>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </BrowserRouter>
   </ClerkProvider>,
-);
+)
