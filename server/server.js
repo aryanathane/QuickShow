e5fcac9158cf448a6cf143987e77c2bde0d2dc58
aryanathane <143585933+aryanathane@users.noreply.github.com>
2025-11-19 10:20:35 +0530

    Day 5 commit 1

--- server/server.js
+++ server/server.js
@@ -9,22 +9,27 @@ import showRouter from './routes/showRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
 import adminRouter from './routes/adminRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import { stripeWebhooks } from './controllers/stripeWebhooks.js';
 
 const app = express()
 const port = 3000
 
 await connectDB()
 
+//Stripe webhook route
+app.use('/api/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
+
+// middleware
 app.use(express.json())
 app.use(cors())
 app.use(clerkMiddleware())
 
+// API routes
 app.get('/',(req,res)=>res.send('server is live!!!!'))
 app.use('/api/inngest',serve({ client: inngest, functions }))
 app.use('/api/show',showRouter)
 app.use('/api/booking', bookingRouter)
 app.use('/api/admin',adminRouter)
 app.use('/api/user',userRouter)
-
 
 app.listen(port, ()=> console.log(`server listening at http:localhost:${port}`) )
